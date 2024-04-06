bf2c52d45d58e18732a8210c157c8dc07c07d686
WAILENGL <162903708+WAILENGL@users.noreply.github.com>
2024-04-06 12:31:10 +0800

    Friday update

--- Worbble Game/main.js
+++ Worbble Game/main.js
@@ -6,7 +6,7 @@ let row = 0; // this is the row player is guessing in or attempt no.
 let col = 0; //  the current letter for the row
 
 let gameOver = false;
-let gameWord = ["retinas"] //one word to test for now, export in wordlist later
+let gameWord = "retinas" //one word to test for now, export in wordlist later
 
 
 window.onload = function(){
@@ -20,7 +20,7 @@ function initialize() {
     for (let r = 0; r < height; r++) {
         for (let c = 0; c < width; c++) {
             // <span id="0-0" class="tile">P</span>
-            let tile = document.createElement("span");
+            let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
             tile.innerText = "";
@@ -36,53 +36,57 @@ document.addEventListener ("keyup", (e) => {
             let currentTile = document.getElementById(row.toString() + '-' + col.toString());
             if (currentTile.innerText === "") {
                 currentTile.innerText = e.code[3];
-                col += 1;
+                col++;
             }
         }
     }
 	else if(e.code === "Backspace"){
-if (0 < col && col <= width) {
-	col -= 1;
-}
-let currentTile = document.getElementById(row.toString() + '-' + col.toString());
-currentTile.innerText = "";
+		if (0 < col && col <= width) {
+				col--;}
+				let currentTile = document.getElementById(row.toString() + '-' + col.toString());
+				currentTile.innerText = "";
 	}
 else if (e.code === "Enter"){
-update();
-row +=1; //start new row
-col = 0; // start at 0 for new row
+	update();
+	row ++; //start new row
+	col = 0; // start at 0 for new row
 }
 
 if (!gameOver && row === height){gameOver = true;
-	document.getElementById("answer").innerText = gameWord;
-	}
-
+	document.getElementById("answer").innerText = "Game Over! The Answer Is " + gameWord.toUpperCase();
+}
 }
 )
 }
 
 function update() {
 let correct = 0;
+let letterCount = new set(gameWord.split(''));
 for (let c = 0; c < width; c++) {
-let currentTile = document.getElementById(row.toString() + '-' + col.toString());
-let letter = currentTile.innerText;
+let currentTile = document.getElementById(row.toString() + '-' + c.toString());
+let letter = currentTile.innerText.toLowerCase();
 
 // is the letter in the correct position?
 
 if (gameWord[c] === letter) {
-tile.classList.add("correct");
-correct += 1;
+currentTile.classList.add("correct");
+correct++;
 
 } // is it in the word? 
 
-if (gameWord.includes.letter) {tile.classList.add("present");
+else if (gameWord.includes(letter)) {currentTile.classList.add("present");
+if (!letterCount.has(letter) && gameWord.indexOf(letter)!== c) {
+	currentTile.classList.add("also-wrong");
+  }
 }
 
-else {tile.classList.add("wrong");
+else {currentTile.classList.add("wrong");
 }
 
 if (correct === width){gameOver = true;}
 
 }
-
 }
+//code in wrong if letter is not repeated
+//code in game winning message
+//code in restart button
